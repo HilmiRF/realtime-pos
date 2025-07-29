3e9b3ceb98a070bde8a7236e1a9a855f3db27c8e
HilmiRF <hilmirf23@gmail.com>
2025-07-29 20:27:56 +0700

    fix: removed unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { DarkModeToggle } from "@/components/commons/darkmode-toggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import Image from "next/image";
 
 export default function Home() {
 	return (
